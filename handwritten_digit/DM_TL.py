fixed gradient bug

handwritten_digit/DM_TL.py
@@ -5,13 +5,14 @@ import lasagne
 from nolearn.lasagne import TrainSplit
 import theano.tensor as T
 import matplotlib.pyplot as plt
-from lasagne.layers import InputLayer, DenseLayer
+from lasagne.layers import InputLayer, DenseLayer, get_all_params
 from lasagne.updates import nesterov_momentum
 from nolearn.lasagne import visualize
 import os
-from NeuralNet import NeuralNet
+import theano
 from theano.tensor import dot, inv
-
+from NeuralNet import NeuralNet
+from CutLayer import CutLayer
 
 def read_and_split(filepath, split_ratio, num):
     df = pd.read_csv(filepath)
@@ -38,18 +39,26 @@ def multilabel_objective(predictions, targets):
     one = np.float32(1.0)
     return -T.sum(targets*T.log(predictions) + (one-targets)*T.log(one-predictions), axis=1)
 
+# def importance(W, last_layer, init):
+#     cur_shared = []
+#     (ni, no) = W.shape
+#     for o in range(no):
+#         abs_weight = abs(W[:,o])
+#         norm = sum(abs_weight)
+#         abs_weight = abs_weight / norm
+#         if last_layer is None:
+#             last_layer = init
+#         cur_shared += [np.dot(abs_weight, last_layer)]
+#     shared = sum(cur_shared)/no
+#     return (shared, cur_shared)
+
 def importance(W, last_layer, init):
-    cur_shared = []
-    (ni, no) = W.shape
-    for o in range(no):
-        abs_weight = abs(W[:,o])
-        norm = sum(abs_weight)
-        abs_weight = abs_weight / norm
-        if last_layer is None:
-            last_layer = init
-        cur_shared += [np.dot(abs_weight, last_layer)]
-    shared = sum(cur_shared)/no
-    return (shared, cur_shared)
+    W = np.abs(W)
+    norm = W.sum(axis=0)
+    W = W/norm
+    if last_layer is None:
+        last_layer = init
+    return T.dot(W, last_layer)
 
 def select_mono(shared):
     change = [(shared[i]-shared[i-1]) for i in range(1,len(shared))]
@@ -104,54 +113,86 @@ def calc_shared_F(net):
     return shared_all_layer_I, shared_all_layer_O[::-1], shared_I, shared_O[::-1]
 
 
+# def calc_shared(params):
+#     shared_all_layer_I, shared_all_layer_O = [], []
+#     mid_layers = params[1:]
+#     shared_I, shared_O = [], []
+#     last_layer_I, last_layer_O = None, None
+#     init_I = np.array([1 for i in range(28*28)] + [0 for i in range(28*28)])
+#     init_O = np.array([1 for i in range(10)] + [0 for i in range(10)])
+#     Ws = []
+#     # forward
+#     for layer in mid_layers:
+#         Ws += [layer[0].get_value()]
+#         si, cur_shared = importance(Ws[-1], last_layer_I, init_I)
+#         shared_I += [si]
+#         last_layer_I = np.array(cur_shared)
+#         shared_all_layer_I.append(cur_shared)
+#     mid_layers.reverse()
+#     Ws.reverse()
+#     # backward
+#     for l in range(len(mid_layers)):
+#         W = Ws[l]
+#         invW = dot(W.T, inv(dot(W, W.T)))
+#         so, cur_shared = importance(invW.eval(), last_layer_O, init_O)
+#         shared_O += [so]
+#         last_layer_O = np.array(cur_shared)
+#         shared_all_layer_O.append(cur_shared)
+#     # select monotonically increased ones
+#     shared_I, shared_O = select_mono(shared_I), select_mono(shared_O)
+#     return shared_all_layer_I, shared_all_layer_O[::-1], shared_I, shared_O[::-1]
+
+# def custom_regularizor(layers):
+#     params = [x.get_params() for x in layers.values()]
+#     SI, SO, si, so = calc_shared(params)
+#     SI = np.array([np.var(x) for x in SI])
+#     SO = np.array([np.var(x) for x in SO])
+#     SI = np.exp(sum([SI[x]-SI[x-1] for x in range(1,len(SI))]))
+#     SO = np.exp(sum([SO[x-1]-SO[x-1] for x in range(1,len(SO))]))
+#     print(SI)
+#     print(SO)
+#     return (SI+SO)*LAMBDA
+
+
 def calc_shared(params):
     shared_all_layer_I, shared_all_layer_O = [], []
     mid_layers = params[1:]
-    shared_I, shared_O = [], []
     last_layer_I, last_layer_O = None, None
-    init_I = np.array([1 for i in range(28*28)] + [0 for i in range(28*28)])
-    init_O = np.array([1 for i in range(10)] + [0 for i in range(10)])
+    init_I = theano.shared(np.array([1 for i in range(28*28)] + [0 for i in range(28*28)], dtype=theano.config.floatX))
+    init_O = theano.shared(np.array([1 for i in range(10)] + [0 for i in range(10)], dtype=theano.config.floatX))
     Ws = []
     # forward
     for layer in mid_layers:
-        Ws += [layer[0].get_value()]
-        si, cur_shared = importance(Ws[-1], last_layer_I, init_I)
-        shared_I += [si]
-        last_layer_I = np.array(cur_shared)
-        shared_all_layer_I.append(cur_shared)
+        Ws += [layer]
+        cur_shared = importance(Ws[-1], last_layer_I, init_I)
+        last_layer_I = cur_shared
+        shared_all_layer_I.append(T.var(cur_shared))
     mid_layers.reverse()
     Ws.reverse()
     # backward
     for l in range(len(mid_layers)):
         W = Ws[l]
         invW = dot(W.T, inv(dot(W, W.T)))
-        so, cur_shared = importance(invW.eval(), last_layer_O, init_O)
-        shared_O += [so]
-        last_layer_O = np.array(cur_shared)
-        shared_all_layer_O.append(cur_shared)
-    # select monotonically increased ones
-    shared_I, shared_O = select_mono(shared_I), select_mono(shared_O)
-    return shared_all_layer_I, shared_all_layer_O[::-1], shared_I, shared_O[::-1]
-
+        cur_shared = importance(invW.eval(), last_layer_O, init_O)
+        last_layer_O = cur_shared
+        shared_all_layer_O.append(T.var(cur_shared))
+    return shared_all_layer_I, shared_all_layer_O[::-1]
+    
 def custom_regularizor(layers):
-    params = [x.get_params() for x in layers.values()]
-    SI, SO, si, so = calc_shared(params)
-    #SI = np.array([np.amax(x)-np.amin(x) for x in SI])
-    #SO = np.array([np.amax(x)-np.amax(x) for x in SO])
-    SI = np.array([np.var(x) for x in SI])
-    SO = np.array([np.var(x) for x in SO])
-    #L = np.array(range(1,len(layers)))
-    #IL = len(layers)-L+1
-    #reg = -(dot(SI,np.exp(IL))+dot(SO, np.exp(L)))
-    SI = np.exp(sum([SI[x]-SI[x-1] for x in range(1,len(SI))]))
-    SO = np.exp(sum([SO[x-1]-SO[x-1] for x in range(1,len(SO))]))
+    params = get_all_params(layers.values())
+    params = [params[i] for i in range(0,len(params), 2)]
+    print(params)
+    SI, SO = calc_shared(params)
     print(SI)
     print(SO)
+    SI = np.exp(sum([SI[x]-SI[x-1] for x in range(1,len(SI))]))
+    SO = np.exp(sum([SO[x-1]-SO[x-1] for x in range(1,len(SO))]))
     return (SI+SO)*LAMBDA
 
 def control_layer_num(n, l):
     tl = l
     for i in range(n):
+        tl = CutLayer(tl, p=0.5)
         tl = DenseLayer(tl, num_units=HN, nonlinearity=None)
     return tl
 

handwritten_digit/NeuralNet.py
@@ -391,6 +391,7 @@ class NeuralNet(BaseEstimator):
             accuracy = loss_eval
 
         all_params = self.get_all_params(trainable=True)
+        theano.printing.pydotprint(loss_train, outfile="grad.png", var_with_name_simple=True)
         grads = theano.grad(loss_train, all_params)
         for idx, param in enumerate(all_params):
             grad_scale = getattr(param.tag, 'grad_scale', 1)
@@ -445,11 +446,11 @@ class NeuralNet(BaseEstimator):
         return self.fit(X, y, epochs=1)
 
     def train_loop(self, X, y, epochs=None):
-        bad = True
-        while bad:
+        nanF, abF = True, False
+        while nanF:
             epochs = epochs or self.max_epochs
             X_train, X_valid, y_train, y_valid = self.train_split(X, y, self)
-
+            # reset [later]
             on_batch_finished = self.on_batch_finished
             if not isinstance(on_batch_finished, (list, tuple)):
                 on_batch_finished = [on_batch_finished]
@@ -527,22 +528,26 @@ class NeuralNet(BaseEstimator):
                     'dur': time() - t0,
                 }
                 # custom cut
-                if (avg_valid_loss > 15 and epoch > 20) or np.isnan(avg_valid_loss):
+                if (avg_valid_loss > 20 and epoch > 20) or np.isnan(avg_valid_loss):
+                    abF = True
                     break
                 if self.custom_scores:
                     for index, custom_score in enumerate(self.custom_scores):
                         info[custom_score[0]] = avg_custom_scores[index]
                 self.train_history_.append(info)
-
                 try:
                     for func in on_epoch_finished:
                         func(self, self.train_history_)
                 except StopIteration:
                     break
-
+            if abF:
+                nanF, abF = True, False
+                continue
+            else:
+                break
             for func in on_training_finished:
                 func(self, self.train_history_)
-            bad = False
+            
 
     @staticmethod
     def apply_batch_func(func, Xb, yb=None):